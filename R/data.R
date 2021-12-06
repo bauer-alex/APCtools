41aa0c34791128781854cb0fdb3fe4a53e4833cb
bauer-alex <alexander.bauer@stat.uni-muenchen.de>
2021-12-06 14:58:38 +0100

    renamed 'data_RA' (-> 'travel'), included dataset 'drug_deaths', data documentation

--- R/data.R
+++ R/data.R
@@ -18,31 +18,31 @@
 #' 
 #' @docType data
 #' 
-#' @usage data(data_RA)
+#' @usage data(travel)
 #' 
 #' @format A dataframe containing
 #' \describe{
-#'  \item{period}{Year in which the respondent traveled.}
-#'  \item{age}{Age of the respondent.}
-#'  \item{sampling_weight}{Individual weight of each respondent to account for
-#'  a not perfectly representative sample and project the sample results to
-#'  the population of German citizens (until 2009) or of German-speaking
-#'  residents (starting 2010). Only available since 1974.}
-#'  \item{german_citizenship}{Indicator if the respondent is German citizen or
-#'  not. Only available since 2010. Until 2009, all respondents were German
-#'  citizens.}
-#'  \item{residence_region}{Indicator if the respondent's main residence is in a
-#'  federal state in the former area of West Germany or in the former area of
-#'  East Germany.}
-#'  \item{household size}{Categorized size of the respondent's household.}
-#'  \item{household_income}{Joint income (in €) of the respondent's household.}
-#'  \item{mainTrip_duration}{Categorized trip length of the respondent's
-#'  \emph{main trip}. The main trip is the trip which the respondent stated was
-#'  his/her most important trip in the respective year.}
-#'  \item{mainTrip_distance}{Distance (in km) between the center of the
-#'  respondent's federal state and the center of the country of destination,
-#'  for the \emph{main trip}. The main trip is the trip which the respondent
-#'  stated was his/her most important trip in the respective year.}
+#'   \item{period}{Year in which the respondent traveled.}
+#'   \item{age}{Age of the respondent.}
+#'   \item{sampling_weight}{Individual weight of each respondent to account for
+#'   a not perfectly representative sample and project the sample results to
+#'   the population of German citizens (until 2009) or of German-speaking
+#'   residents (starting 2010). Only available since 1974.}
+#'   \item{german_citizenship}{Indicator if the respondent is German citizen or
+#'   not. Only available since 2010. Until 2009, all respondents were German
+#'   citizens.}
+#'   \item{residence_region}{Indicator if the respondent's main residence is in a
+#'   federal state in the former area of West Germany or in the former area of
+#'   East Germany.}
+#'   \item{household size}{Categorized size of the respondent's household.}
+#'   \item{household_income}{Joint income (in €) of the respondent's household.}
+#'   \item{mainTrip_duration}{Categorized trip length of the respondent's
+#'   \emph{main trip}. The main trip is the trip which the respondent stated was
+#'   his/her most important trip in the respective year.}
+#'   \item{mainTrip_distance}{Distance (in km) between the center of the
+#'   respondent's federal state and the center of the country of destination,
+#'   for the \emph{main trip}. The main trip is the trip which the respondent
+#'   stated was his/her most important trip in the respective year.}
 #' }
 #' 
 #' @references Weigert, M., Bauer, A., Gernert, J., Karl, M., Nalmpatian, A., Küchenhoff,
@@ -58,4 +58,33 @@
 #' 
 #' @keywords datasets
 #' 
-"data_RA"
+"travel"
+
+
+
+#' Drug deaths of white men in the United States
+#' 
+#' TODO detailed description, including how to retrieve the data from the online portal
+#' 
+#' @docType data
+#' 
+#' @usage data(data_RA)
+#' 
+#' @format A dataframe containing
+#' \describe{
+#'   \item{period}{TODO.}
+#'   \item{age}{TODO.}
+#'   \item{deaths}{TODO.}
+#'   \item{population}{TODO.}
+#'   \item{death_rate}{TODO.}
+#' }
+#' 
+#' @references
+#' TODO format both references
+#' Jalal, H., & Burke, D. S. (2020). Hexamaps for Age-Period-Cohort Data Visualization and Implementation in R. Epidemiology (Cambridge, Mass.), 31(6), e47.
+#' 
+#' Centers for Disease Control and Prevention, National Center for Health Statistics. Underlying Cause of Death 1999-2019 on CDC WONDER Online Database, released in 2020. Data are from the Multiple Cause of Death Files, 1999-2019, as compiled from data provided by the 57 vital statistics jurisdictions through the Vital Statistics Cooperative Program. Accessed at http://wonder.cdc.gov/ucd-icd10.html on Dec 6, 2021 6:03:26 AM
+#' 
+#' @keywords datasets
+#' 
+"drug_deaths"
--- data-raw/save_dataObjects.R
+++ data-raw/save_dataObjects.R
@@ -1,9 +1,32 @@
 
+library(dplyr)
 
-# load the data from the 'data-raw' folder --------------------------------
-data_RA <- readRDS("Reiseanalyse_sample.rds")
+
+# read the data from the 'data-raw' folder --------------------------------
+# travel behavior of German travelers
+travel <- readRDS("Reiseanalyse_sample.rds")
+
+# white male drug deaths in the United States
+drug_deaths <- read.table("usa_drugDeaths_1999_2019.txt", sep = "\t",
+                          header = TRUE, nrows = 2142,
+                          na.strings = c("NS","Not Applicable","Unreliable"))
+
+
+
+# further data preparation ------------------------------------------------
+drug_deaths <- drug_deaths %>% 
+  select(Year, Single.Year.Ages.Code, Deaths, Population, Crude.Rate) %>% 
+  dplyr::rename(period     = Year,
+                age        = Single.Year.Ages.Code,
+                deaths     = Deaths,
+                population = Population,
+                death_rate = Crude.Rate) %>% 
+  filter(!is.na(age)) %>% 
+  mutate(age        = as.integer(age),
+         population = as.integer(population),
+         death_rate = as.integer(death_rate))
 
 
 
 # save the data in the package --------------------------------------------
-usethis::use_data(data_RA, overwrite = TRUE)
+usethis::use_data(travel, drug_deaths, overwrite = TRUE)
